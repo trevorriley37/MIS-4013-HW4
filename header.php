8b8dcc31bf9f414627b7ddcf1d030dc29fcdba75
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-12 13:44:41 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,60 @@
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php
+$servername = "localhost";
+$username = "trileyou_rile0056";
+$password = "T833481t!";
+$dbname = "trileyou_database1";
+  
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Cities</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="basketball.php">Basketball</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="football.php">Football</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="baseball.php">Baseball</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="together.php">Together</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="posthandle.php">Post</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
