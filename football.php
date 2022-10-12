fc4f4c8ff329b849a08cd22fb1d3a8c661ad4fdb
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-12 13:57:50 -0500

    all pages back

---
+++ football.php
@@ -0,0 +1,42 @@
+<?php require_once("header.php"); ?>
+
+
+<table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">City ID</th>
+      <th scope="col">Football Team Name</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+
+$sql = "SELECT city_ID,football_name from Football order by city_ID";
+$result = $conn->query($sql);
+
+
+
+
+
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ 
+    ?>
+    <tr>
+     
+   <td><?=$row["city_ID"]?></td>
+    <td><?=$row["football_name"]?></td>
+    </tr>
+ 
+<?php
+  }
+} else {
+  echo "0 results";
+}
+   
+  ?> 
+   </tbody>
+</table>
+</html>
---
+++ together.php
@@ -0,0 +1,54 @@
+<?php require_once("header.php"); ?>
+
+
+
+<table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th>City_ID</th>
+      <th>Abbreviation</th>
+     <th>Name</th>
+     <th>Baseball Team</th>
+     <th>Football Team</th>
+     <th>Basketball Team</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+   
+   //joining all four tables together
+$sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID";
+$result = $conn->query($sql);
+   
+   
+   
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["city_ID"]?></td>
+    <td><?=$row["Abbreviation"]?></td>
+   <td><?=$row["fullname"]?></td>
+    <td><?=$row["Baseball_name"]?></td>
+    <td><?=$row["football_name"]?></td>
+    <td><?=$row["Team_Name"]?></td>
+    
+    
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+ 
+
+
