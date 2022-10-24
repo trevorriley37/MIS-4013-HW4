d138c0c52e6104c900c9ae7356f5f47058551bb1
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-24 17:04:30 -0500

    ok

--- index.php
+++ index.php
@@ -101,9 +101,11 @@ if ($result->num_rows > 0) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
-
 <?php
- while($row = $result->fetch_assoc()) {
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
 ?>
           
           <tr>
@@ -149,7 +151,7 @@ if ($result->num_rows > 0) {
           
 <?php
   }
- else {
+} else {
   echo "0 results";
 }
 $conn->close();
