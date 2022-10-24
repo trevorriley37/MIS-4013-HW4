c423217193cd2096ce5334c13369e723b8c009e9
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-24 12:12:24 -0500

    ok

--- index.php
+++ index.php
@@ -58,18 +58,6 @@ $result = $conn->query($sql);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   
           
-        <thead>
-          <tr>
-            <th>CourseID</th>
-            <th>InstructorID</th>
-            <th>Course</th>
-            <th>Section</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          
 <?php
 
 if ($result->num_rows > 0) {
