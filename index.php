e29e9ed68e1e0b79af1345d9b8515ffe579d4626
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-24 17:01:16 -0500

    maybe

--- index.php
+++ index.php
@@ -101,6 +101,9 @@ if ($result->num_rows > 0) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
+
+<?php
+
    while($row = $result->fetch_assoc()) {
 ?>
           
