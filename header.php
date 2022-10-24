7ddec4880fc78ce4c16f4c00c89d24860d437080
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-24 13:06:24 -0500

    want to see

--- header.php
+++ header.php
@@ -55,3 +55,20 @@ echo "Connected successfully";
     </div>
   </div>
 </nav>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+        $sqlAdd = "insert into City (Abbreviation, fullname) value (?, ?)";
+        $stmtAdd = $conn->prepare($sqlAdd);
+        $stmtAdd->bind_param("ss", $_POST['icityabrv'], $_POST['icityname']);
+        $stmtAdd->execute();   
+      echo '<div class="alert alert-success" role="alert">New City added.</div>';
+      break;
+
+ 
+      
+      
+      ?>
--- index.php
+++ index.php
@@ -60,9 +60,9 @@ if ($result->num_rows > 0) {
               <form method="post" action="">
                 <div class="mb-3">
                   <label for="editInstructor<?=$row["InstructorID"]?>Name" class="form-label">First Name</label>
-                          <input type="text" class="form-control" id="editInstructor<?=$row["InstructorID"]?>Name" aria-describedby="editInstructor<?=$row["InstructorID"]?>Help" name="iFirstName">
+                          <input type="text" class="form-control" id="editInstructor<?=$row["InstructorID"]?>Name" aria-describedby="editInstructor<?=$row["InstructorID"]?>Help" name="icityabrv">
                           <label for="editInstructor<?=$row["InstructorID"]?>Name" class="form-label">Last Name</label>
-                          <input type="text" class="form-control" id="editInstructor<?=$row["InstructorID"]?>Name" aria-describedby="editInstructor<?=$row["InstructorID"]?>Help" name="iLastName">
+                          <input type="text" class="form-control" id="editInstructor<?=$row["InstructorID"]?>Name" aria-describedby="editInstructor<?=$row["InstructorID"]?>Help" name="icityname">
                           <div id="editInstructor<?=$row["InstructorID"]?>Help" class="form-text">Enter the instructor's name.</div>
                         </div>
                 <input type="hidden" name="saveType" value="Add">
