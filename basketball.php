b5b79721c1b94414a6fc9a1e50ffbdb1d9778881
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-25 16:01:44 -0500

    this may help idk

--- basketball.php
+++ basketball.php
@@ -114,11 +114,11 @@ if ($result->num_rows > 0) {
                       <form method="post" action="">
                         <div class="mb-3">
                           <label for="editbasketball<?=$row["city_ID"]?>Name" class="form-label"> Enter CityID</label>
-                          <input type="text" class="form-control" id="editbasketball<?=$row["city_ID"]?>Name" aria-describedby="editbasketball<?=$row["city_ID"]?>Help" name="editcityID" value="<?=$row['city_ID']?>">
-                          <label for="editbasketball<?=$row["city_ID"]?>Name" class="form-label"> Enter Team Name</label>
-                          <input type="text" class="form-control" id="editbasketball<?=$row["city_ID"]?>Name" aria-describedby="editbasketball<?=$row["city_ID"]?>Help" name="editteamname" value="<?=$row['Team_Name']?>">
-                          <div id="editbasketball<?=$row["city_ID"]?>Help" class="form-text">Enter the Teams CityID and Name.</div>
-                        </div>
+                             <input type="text" class="form-control" id="editbasketball<?=$row["city_ID"]?>Name" aria-describedby="editbasketball<?=$row["city_ID"]?>Help" name="editcityID" value="<?=$row['city_ID']?>">
+                             <label for="editbasketball<?=$row["city_ID"]?>Name" class="form-label"> Enter Team Name</label>
+                             <input type="text" class="form-control" id="editbasketball<?=$row["city_ID"]?>Name" aria-describedby="editbasketball<?=$row["city_ID"]?>Help" name="editteamname" value="<?=$row['Team_Name']?>">
+                             <div id="editbasketball<?=$row["city_ID"]?>Help" class="form-text">Enter the Teams CityID and Name.</div>
+                            </div>
                         <input type="hidden" name="iid" value="<?=$row['city_ID']?>">
                         <input type="hidden" name="saveType" value="Edit">
                         <button type="submit" class="btn btn-primary">Submit</button>
